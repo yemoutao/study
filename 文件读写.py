08b4bb6677ae2de3c92ccd8be39b4f3b7843cf52
Yemt <yemt@ansiw.com>
2022-06-20 14:28:19 +0800

    update

--- 文件读写.py
+++ 文件读写.py
@@ -2,9 +2,18 @@
 from asyncore import read
 
 
-f = open(file="上门服务", mode="r", encoding="utf-8")
-# conter2 = f.read()
-# print(conter2)
+# f = open(file="上门服务", mode="r", encoding="utf-8")  # 在路径前面加r可以转义
+# # conter2 = f.read()
+# # print(conter2)
 
-d = f.read(6)
-print(d)
+# # d = f.read(7)  # 读取七个字符串
+# # print(d)
+
+# c = f.readline() # 读取第一行的字符串
+# print(c)
+
+# w:  1.先清空文件中的字符串 2. 写入对应字符串
+
+f = open("./测试.txt","r+",encoding="utf-8")
+f.write("这是python文件读写视频中的测试")
+print(f.read())
