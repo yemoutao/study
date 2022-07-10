d54d8d28594ad530282665479b838600cb17dc17
Yemt <yemt@ansiw.com>
2022-07-10 23:53:34 +0800

    update

--- 装饰器.py
+++ 装饰器.py
@@ -2,7 +2,11 @@
 ###   开发封闭原则：1.对额外新增的功能开发
 #                  2.对源代码及调用方式是封闭
 #                  3.@wrapper  语法糖
+#    装饰器的应用场景：1. 面向对象对象
+#                     2. 登录认证
+#                     3.falsk路由全都是有参数装饰器
 
+from os import rename
 import time
 
 
@@ -17,7 +21,7 @@ def run_time(f):
     def inner():
         start_time = time.time()
         f()
-        time.sleep(2)
+        # time.sleep(2)
         stop_time = time.time()
         print(f"运行时间为：{stop_time - start_time}")
     return inner
@@ -83,11 +87,58 @@ def end_plate():
     return ret
 
 @wrapper
-def hero(hero_name):
-    ret = print(f"选择{hero_name}出战，死亡如风，常伴吾身")
+def hero(hero_name,hero_title):
+    ret = print(f"选择{hero_name}出战，{hero_title}")
     return ret
 
-hero("亚索")
+# hero(hero_name="亚索",hero_title="死亡如风，常伴吾身")
+hero("小学生之手","懦弱之举，我绝不姑息")
 
 
 # 有参数的装饰器
+def auth(arg):      # auth(arg) == auth(100)
+    def wrapper(func):    #1.func() == tb(100)
+        def inner(*args,**kwargs):     # tb(100) == inner(100)
+            if arg:
+                print("开始装饰")
+                ret = func(*args,**kwargs) # ret = tb(100)
+                print("装饰成功")
+            else:
+                print("装饰失败")
+                ret = func(*args,**kwargs) # ret = tb(100)
+            return ret                 # return "lv"
+        return inner
+    return wrapper
+
+@auth(100)   #语法糖 == auth(100) -->  tb = auth(100)(tb) --> tb = auth(100)(tb)
+def tb(money):
+    print(f"花了{money}：卖了一个皮包")  #print("花了100：卖了一个皮包")
+    return "lv"
+
+# tb = wrapper(tb)
+tb(100)    # auth(100)tb(100)
+
+
+# 多层参数装饰器套用
+def foo(aa):   # aa = 5
+    def auth(arg):     # arg = False 
+        def wrapper(func):   # func = tb(100)
+            def inner(*args,**kwargs):    
+                if arg:
+                    print("开始装饰")
+                    ret = func(*args,**kwargs) 
+                    print("装饰成功")
+                else:
+                    print("装饰失败")
+                    ret = func(*args,**kwargs) 
+                return ret               
+            return inner
+        return wrapper
+    return auth
+
+fee = foo(5)  
+@fee(False)   # == fee(5)auth(False)tb
+def tb(money,beizhu):
+    print(f"花了{money}块钱：{beizhu}")  
+
+tb("1","卖出了一颗棒棒糖")   # == fee(5)auth(False)tb(100)
