e72f34cb2d7ae2735af69d0ee07ada053acd4de5
Yemt <yemt@ansiw.com>
2022-09-12 22:14:49 +0800

    update

---
+++ 随机密码复杂.py
@@ -0,0 +1,17 @@
+import random
+# 应用场景：随机复杂密码
+lst2 = [str(i) for i in range(10)] + [chr(i) for i in range(65,91)] + [chr(i) for i in range(97,123)]
+new_lst = random.sample(lst2,k=3)
+fzpaswwd = "".join(new_lst)
+print(fzpaswwd)
+
+num = [str(i) for i in range(10)]   #数字1-0
+xxzm = [chr(i) for i in range(65,91)]
+dxzm = [chr(i) for i in range(97,123)]
+num1 = random.sample(num,k=1)
+xxzm1 = random.sample(xxzm,k=1)
+dxzm1 = random.sample(dxzm,k=1)
+sjzm =  random.sample('ASDFGHJKLZXCVBNMQWERTYUIOPabcdefghijklmnopqrstuvwxyzzyxwvu1234567890qponmlkjihgfedcba',8)
+passwdnum =num1 + xxzm1 + dxzm1 + sjzm 
+sjpasswd = "".join(passwdnum)
+print(sjpasswd)
